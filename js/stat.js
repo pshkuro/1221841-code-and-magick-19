0fa4d054b10892cae05a257702449915f8008388
Полина <polina110297@yandex.ru>
2020-01-22 21:59:01 +0300

    Учебный проект: холст

--- js/stat.js
+++ js/stat.js
@@ -8,7 +8,6 @@ var GAP = 15;
 var FONT_GAP = 15; // междустрочный интервал
 var HELLO_HEIGHT = 50; // высота блока верхнего текста
 var TEXT_HEIGHT = 20; // высота текста
-// var HISTO_HEIGHT = 150; // высота гистограммы
 var barHeight = CLOUD_HEIGHT - GAP - HELLO_HEIGHT - TEXT_HEIGHT - TEXT_HEIGHT - GAP; // высота колонки
 var BAR_WIDTH = 40; // ширина колонки
 var BAR_GAP = 50; // расстояние между колонками
@@ -32,7 +31,7 @@ var getMaxElement = function (arr) {
 
 var calculateAvailableSpace = function (maxHeight, time, maxTime) {
   return (maxHeight - (maxHeight * time / maxTime));
-}
+};
 
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
@@ -45,19 +44,24 @@ window.renderStatistics = function (ctx, players, times) {
 
   var maxTime = getMaxElement(times);
 
-  for (var k = 0; k < times.length; k++) {
-    ctx.fillText(Math.round(times[k]), CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * k, CLOUD_Y + GAP + HELLO_HEIGHT);
-  }
-
   for (var i = 0; i < players.length; i++) {
+    var AVAILABLE_SPACE = calculateAvailableSpace(barHeight, times[i], maxTime);
+
+    // Время игроков
+    ctx.fillStyle = 'black';
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + GAP + HELLO_HEIGHT + AVAILABLE_SPACE + TEXT_HEIGHT);
+
+    // Имена игроков
+    ctx.fillText(players[i], CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + GAP + HELLO_HEIGHT + TEXT_HEIGHT + TEXT_HEIGHT + barHeight);
+
+    // Гистограмма
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'blue';
     }
 
-    ctx.fillText(players[i], CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + GAP + HELLO_HEIGHT + TEXT_HEIGHT + barHeight + TEXT_HEIGHT);
-    ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + HELLO_HEIGHT + TEXT_HEIGHT + (barHeight - (barHeight * times[i] / maxTime)), BAR_WIDTH, (barHeight * times[i] / maxTime));
+    ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + HELLO_HEIGHT + TEXT_HEIGHT + AVAILABLE_SPACE + TEXT_HEIGHT, BAR_WIDTH, (barHeight * times[i] / maxTime));
   }
 
 
