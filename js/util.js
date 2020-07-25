3d3bfc02f14766746a916473b3cd28dfd90e1df6
Полина <polina110297@yandex.ru>
2020-07-25 13:44:33 +0300

    Refactoring

--- js/util.js
+++ js/util.js
@@ -2,7 +2,6 @@
 
 (function () {
 
-  // Функция генерации случ эл заданного массива
   window.getRandomArrayItem = function (arr) {
     var index = Math.floor(Math.random() * arr.length);
     return arr[index];
